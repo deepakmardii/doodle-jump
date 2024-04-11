26bb18c103700ec1851a2e9c50293cec17ac6698
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-11 20:21:35 +0530

    doodler moving left and right

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ let doodler = {
   height: doodlerHeight,
 };
 
+//physics
+let velocityX = 0;
+let velocityY = 0; //doodler jump speed
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
@@ -43,9 +47,10 @@ window.onload = function () {
   };
 
   doodlerLeftImg = new Image();
-  doodlerLeftImg.src = "./doodler-left.png";
+  doodlerLeftImg.src = "./src/doodler-left.png";
 
   requestAnimationFrame(update);
+  document.addEventListener("keydown", moveDoodler);
 };
 
 function update() {
@@ -57,4 +62,19 @@ function update() {
     doodler.width,
     doodler.height
   );
+
+  //doodler
+  doodler.x += velocityX;
+}
+
+function moveDoodler(e) {
+  if (e.code == "ArrowRight" || e.code == "KeyD") {
+    //move right
+    velocityX = 4;
+    doodler.img = doodlerRightImg;
+  } else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+    //move left
+    velocityX = -4;
+    doodler.img = doodlerLeftImg;
+  }
 }
