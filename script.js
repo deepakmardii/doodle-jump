1d133d7c37a64a79ac0475357a7f316c5344fd0d
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-11 20:04:43 +0530

    board dimension

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 //board
 let board;
-
 let boardWidth = 360; //from image
 let boardHeight = 576;
-
 let context;
+
+//doodler
+let doodlerHeight = 46;
+let doodlerWidth = 46;
+let doodlerX = boardWidth / 2 - doodlerWidth / 2;
+let doodlerY = (boardHeight * 7) / 8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+  img: null,
+  x: doodlerX,
+  y: doodlerY,
+  width: doodlerWidth,
+  height: doodlerHeight,
+};
 
 window.onload = function () {
   board = document.getElementById("board");
